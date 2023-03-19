c7ace961949877c93d2119f7121538ae41030434
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-03-19 13:05:43 +0900

    split the task

--- Plotly/main/main.c
+++ Plotly/main/main.c
@@ -18,23 +18,11 @@
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-#include "cJSON.h"
 #include "mdns.h"
-
-#include "soc/adc_channel.h"
-#if ESP_IDF_VERSION_MAJOR == 5 && ESP_IDF_VERSION_MINOR == 0
-#include "driver/adc.h" //  Need legacy adc driver for ADC1_GPIOxx_CHANNEL
-#endif
-#include "esp_adc/adc_cali.h"
-#include "esp_adc/adc_cali_scheme.h"
-#include "esp_adc/adc_oneshot.h"
 
 #include "websocket_server.h"
 
-static QueueHandle_t client_queue;
-MessageBufferHandle_t xMessageBufferMain;
-
-const static int client_queue_size = 10;
+MessageBufferHandle_t xMessageBufferToClient;
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -154,324 +142,11 @@ void initialise_mdns(void)
 #endif
 }
 
-// handles websocket events
-void websocket_callback(uint8_t num,WEBSOCKET_TYPE_t type,char* msg,uint64_t len) {
-	const static char* TAG = "websocket_callback";
-	//int value;
-
-	switch(type) {
-		case WEBSOCKET_CONNECT:
-			ESP_LOGI(TAG,"client %i connected!",num);
-			break;
-		case WEBSOCKET_DISCONNECT_EXTERNAL:
-			ESP_LOGI(TAG,"client %i sent a disconnect message",num);
-			break;
-		case WEBSOCKET_DISCONNECT_INTERNAL:
-			ESP_LOGI(TAG,"client %i was disconnected",num);
-			break;
-		case WEBSOCKET_DISCONNECT_ERROR:
-			ESP_LOGI(TAG,"client %i was disconnected due to an error",num);
-			break;
-		case WEBSOCKET_TEXT:
-			if(len) { // if the message length was greater than zero
-				ESP_LOGI(TAG, "got message length %i: %s", (int)len, msg);
-				size_t xBytesSent = xMessageBufferSendFromISR(xMessageBufferMain, msg, len, NULL);
-				if (xBytesSent != len) {
-					ESP_LOGE(TAG, "xMessageBufferSend fail");
-				}
-			}
-			break;
-		case WEBSOCKET_BIN:
-			ESP_LOGI(TAG,"client %i sent binary message of size %"PRIu32":\n%s",num,(uint32_t)len,msg);
-			break;
-		case WEBSOCKET_PING:
-			ESP_LOGI(TAG,"client %i pinged us with message of size %"PRIu32":\n%s",num,(uint32_t)len,msg);
-			break;
-		case WEBSOCKET_PONG:
-			ESP_LOGI(TAG,"client %i responded to the ping",num);
-			break;
-	}
-}
-
-// serves any clients
-static void http_serve(struct netconn *conn) {
-	const static char* TAG = "http_server";
-	const static char HTML_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
-	const static char ERROR_HEADER[] = "HTTP/1.1 404 Not Found\nContent-type: text/html\n\n";
-	const static char JS_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/javascript\n\n";
-	const static char CSS_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/css\n\n";
-	//const static char PNG_HEADER[] = "HTTP/1.1 200 OK\nContent-type: image/png\n\n";
-	const static char ICO_HEADER[] = "HTTP/1.1 200 OK\nContent-type: image/x-icon\n\n";
-	//const static char PDF_HEADER[] = "HTTP/1.1 200 OK\nContent-type: application/pdf\n\n";
-	//const static char EVENT_HEADER[] = "HTTP/1.1 200 OK\nContent-Type: text/event-stream\nCache-Control: no-cache\nretry: 3000\n\n";
-	struct netbuf* inbuf;
-	static char* buf;
-	static uint16_t buflen;
-	static err_t err;
-
-	// default page
-	extern const uint8_t root_html_start[] asm("_binary_root_html_start");
-	extern const uint8_t root_html_end[] asm("_binary_root_html_end");
-	const uint32_t root_html_len = root_html_end - root_html_start;
-
-	// main.js
-	extern const uint8_t main_js_start[] asm("_binary_main_js_start");
-	extern const uint8_t main_js_end[] asm("_binary_main_js_end");
-	const uint32_t main_js_len = main_js_end - main_js_start;
-
-	// main.css
-	extern const uint8_t main_css_start[] asm("_binary_main_css_start");
-	extern const uint8_t main_css_end[] asm("_binary_main_css_end");
-	const uint32_t main_css_len = main_css_end - main_css_start;
-
-	// favicon.ico
-	extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
-	extern const uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
-	const uint32_t favicon_ico_len = favicon_ico_end - favicon_ico_start;
-
-	// error page
-	extern const uint8_t error_html_start[] asm("_binary_error_html_start");
-	extern const uint8_t error_html_end[] asm("_binary_error_html_end");
-	const uint32_t error_html_len = error_html_end - error_html_start;
-
-	netconn_set_recvtimeout(conn,1000); // allow a connection timeout of 1 second
-	ESP_LOGI(TAG,"reading from client...");
-	err = netconn_recv(conn, &inbuf);
-	ESP_LOGI(TAG,"read from client");
-	if(err==ERR_OK) {
-		netbuf_data(inbuf, (void**)&buf, &buflen);
-		if(buf) {
-
-			ESP_LOGD(TAG, "buf=[%s]", buf);
-			// default page
-			if		 (strstr(buf,"GET / ")
-					&& !strstr(buf,"Upgrade: websocket")) {
-				ESP_LOGI(TAG,"Sending /");
-				netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER)-1,NETCONN_NOCOPY);
-				netconn_write(conn, root_html_start,root_html_len,NETCONN_NOCOPY);
-				netconn_close(conn);
-				netconn_delete(conn);
-				netbuf_delete(inbuf);
-			}
-
-			// default page websocket
-			else if(strstr(buf,"GET / ")
-					 && strstr(buf,"Upgrade: websocket")) {
-				ESP_LOGI(TAG,"Requesting websocket on /");
-				ws_server_add_client(conn,buf,buflen,"/",websocket_callback);
-				netbuf_delete(inbuf);
-			}
-
-			else if(strstr(buf,"GET /main.js ")) {
-				ESP_LOGI(TAG,"Sending /main.js");
-				netconn_write(conn, JS_HEADER, sizeof(JS_HEADER)-1,NETCONN_NOCOPY);
-				netconn_write(conn, main_js_start, main_js_len,NETCONN_NOCOPY);
-				netconn_close(conn);
-				netconn_delete(conn);
-				netbuf_delete(inbuf);
-			}
-
-			else if(strstr(buf,"GET /main.css ")) {
-				ESP_LOGI(TAG,"Sending /main.css");
-				netconn_write(conn, CSS_HEADER, sizeof(CSS_HEADER)-1,NETCONN_NOCOPY);
-				netconn_write(conn, main_css_start, main_css_len,NETCONN_NOCOPY);
-				netconn_close(conn);
-				netconn_delete(conn);
-				netbuf_delete(inbuf);
-			}
-
-			else if(strstr(buf,"GET /favicon.ico ")) {
-				ESP_LOGI(TAG,"Sending favicon.ico");
-				netconn_write(conn,ICO_HEADER,sizeof(ICO_HEADER)-1,NETCONN_NOCOPY);
-				netconn_write(conn,favicon_ico_start,favicon_ico_len,NETCONN_NOCOPY);
-				netconn_close(conn);
-				netconn_delete(conn);
-				netbuf_delete(inbuf);
-			}
-
-			else if(strstr(buf,"GET /")) {
-				ESP_LOGE(TAG,"Unknown request, sending error page: %s",buf);
-				netconn_write(conn, ERROR_HEADER, sizeof(ERROR_HEADER)-1,NETCONN_NOCOPY);
-				netconn_write(conn, error_html_start, error_html_len,NETCONN_NOCOPY);
-				netconn_close(conn);
-				netconn_delete(conn);
-				netbuf_delete(inbuf);
-			}
-
-			else {
-				ESP_LOGE(TAG,"Unknown request");
-				netconn_close(conn);
-				netconn_delete(conn);
-				netbuf_delete(inbuf);
-			}
-		}
-		else {
-			ESP_LOGI(TAG,"Unknown request (empty?...)");
-			netconn_close(conn);
-			netconn_delete(conn);
-			netbuf_delete(inbuf);
-		}
-	}
-	else { // if err==ERR_OK
-		ESP_LOGI(TAG,"error on read, closing connection");
-		netconn_close(conn);
-		netconn_delete(conn);
-		netbuf_delete(inbuf);
-	}
-}
-
-// handles clients when they first connect. passes to a queue
-static void server_task(void* pvParameters) {
-	const static char* TAG = "server_task";
-	char *task_parameter = (char *)pvParameters;
-	ESP_LOGI(TAG, "Start task_parameter=%s", task_parameter);
-	char url[64];
-	sprintf(url, "http://%s", task_parameter);
-	ESP_LOGI(TAG, "Starting server on %s", url);
-
-	struct netconn *conn, *newconn;
-	static err_t err;
-	client_queue = xQueueCreate(client_queue_size,sizeof(struct netconn*));
-	configASSERT( client_queue );
-
-	conn = netconn_new(NETCONN_TCP);
-	netconn_bind(conn,NULL,80);
-	netconn_listen(conn);
-	ESP_LOGI(TAG,"server listening");
-	do {
-		err = netconn_accept(conn, &newconn);
-		ESP_LOGI(TAG,"new client");
-		if(err == ERR_OK) {
-			xQueueSendToBack(client_queue,&newconn,portMAX_DELAY);
-			//http_serve(newconn);
-		}
-	} while(err == ERR_OK);
-	netconn_close(conn);
-	netconn_delete(conn);
-	ESP_LOGE(TAG,"task ending, rebooting board");
-	esp_restart();
-}
-
-// receives clients from queue, handles them
-static void server_handle_task(void* pvParameters) {
-	const static char* TAG = "server_handle_task";
-	struct netconn* conn;
-	ESP_LOGI(TAG,"task starting");
-	for(;;) {
-		xQueueReceive(client_queue,&conn,portMAX_DELAY);
-		if(!conn) continue;
-		http_serve(conn);
-	}
-	vTaskDelete(NULL);
-}
-
-// Timer callback
-static void timer_cb(TimerHandle_t xTimer)
-{
-	TickType_t nowTick;
-	nowTick = xTaskGetTickCount();
-	ESP_LOGD(TAG, "timer is called, now=%"PRIu32, nowTick);
-
-	cJSON *request;
-	request = cJSON_CreateObject();
-	cJSON_AddStringToObject(request, "id", "timer-request");
-	char *my_json_string = cJSON_Print(request);
-	ESP_LOGD(TAG, "my_json_string\n%s",my_json_string);
-	size_t xBytesSent = xMessageBufferSendFromISR(xMessageBufferMain, my_json_string, strlen(my_json_string), NULL);
-	if (xBytesSent != strlen(my_json_string)) {
-		ESP_LOGE(TAG, "xMessageBufferSend fail");
-	}
-	cJSON_Delete(request);
-	cJSON_free(my_json_string);
-}
-
-// convert from gpio to adc1 channel
-adc_channel_t gpio2adc(int gpio) {
-#if CONFIG_IDF_TARGET_ESP32
-	if (gpio == 32) return ADC1_GPIO32_CHANNEL;
-	if (gpio == 33) return ADC1_GPIO33_CHANNEL;
-	if (gpio == 34) return ADC1_GPIO34_CHANNEL;
-	if (gpio == 35) return ADC1_GPIO35_CHANNEL;
-	if (gpio == 36) return ADC1_GPIO36_CHANNEL;
-	if (gpio == 37) return ADC1_GPIO37_CHANNEL;
-	if (gpio == 38) return ADC1_GPIO38_CHANNEL;
-	if (gpio == 39) return ADC1_GPIO39_CHANNEL;
-
-#elif CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
-	if (gpio == 1) return ADC1_GPIO1_CHANNEL;
-	if (gpio == 2) return ADC1_GPIO2_CHANNEL;
-	if (gpio == 3) return ADC1_GPIO3_CHANNEL;
-	if (gpio == 4) return ADC1_GPIO4_CHANNEL;
-	if (gpio == 5) return ADC1_GPIO5_CHANNEL;
-	if (gpio == 6) return ADC1_GPIO6_CHANNEL;
-	if (gpio == 7) return ADC1_GPIO7_CHANNEL;
-	if (gpio == 8) return ADC1_GPIO8_CHANNEL;
-	if (gpio == 9) return ADC1_GPIO9_CHANNEL;
-	if (gpio == 10) return ADC1_GPIO10_CHANNEL;
-
-#elif CONFIG_IDF_TARGET_ESP32C2 || CONFIG_IDF_TARGET_ESP32C3
-	if (gpio == 0) return ADC1_GPIO0_CHANNEL;
-	if (gpio == 1) return ADC1_GPIO1_CHANNEL;
-	if (gpio == 2) return ADC1_GPIO2_CHANNEL;
-	if (gpio == 3) return ADC1_GPIO3_CHANNEL;
-	if (gpio == 4) return ADC1_GPIO4_CHANNEL;
-
-#endif
-	return -1;
-}
-
-static bool adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
-{
-	adc_cali_handle_t handle = NULL;
-	esp_err_t ret = ESP_FAIL;
-	bool calibrated = false;
-
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-	if (!calibrated) {
-		ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
-		adc_cali_curve_fitting_config_t cali_config = {
-			.unit_id = unit,
-			.atten = atten,
-			.bitwidth = ADC_BITWIDTH_DEFAULT,
-		};
-		ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
-		if (ret == ESP_OK) {
-			calibrated = true;
-		}
-	}
-#endif
-
-#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-	if (!calibrated) {
-		ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
-		adc_cali_line_fitting_config_t cali_config = {
-			.unit_id = unit,
-			.atten = atten,
-			.bitwidth = ADC_BITWIDTH_DEFAULT,
-		};
-		ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
-		if (ret == ESP_OK) {
-			calibrated = true;
-		}
-	}
-#endif
-
-	*out_handle = handle;
-	if (ret == ESP_OK) {
-		ESP_LOGI(TAG, "Calibration Success");
-	} else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-		ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-	} else {
-		ESP_LOGE(TAG, "Invalid arg or no memory");
-	}
-
-	return calibrated;
-}
-
-#define NO_OF_SAMPLES 64 //Multisampling
+void client_task(void* pvParameters);
+void server_task(void* pvParameters);
 
 void app_main() {
-	//Initialize NVS
+	// Initialize NVS
 	esp_err_t ret = nvs_flash_init();
 	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
 		ESP_ERROR_CHECK(nvs_flash_erase());
@@ -479,161 +154,30 @@ void app_main() {
 	}
 	ESP_ERROR_CHECK(ret);
 
-	ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+	// Initialize WiFi
 	wifi_init_sta();
+
+	// Initialize mDNS
 	initialise_mdns();
 
 	// Create Message Buffer
-	xMessageBufferMain = xMessageBufferCreate(1024);
-	configASSERT( xMessageBufferMain );
+	xMessageBufferToClient = xMessageBufferCreate(1024);
+	configASSERT( xMessageBufferToClient );
 
-	/* Get the local IP address */
+	// Get the local IP address
 	esp_netif_ip_info_t ip_info;
 	ESP_ERROR_CHECK(esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info));
 	char cparam0[64];
 	sprintf(cparam0, IPSTR, IP2STR(&ip_info.ip));
 
+	// Start web socket server
 	ws_server_start();
-	xTaskCreate(&server_task, "server_task", 1024*2, (void *)cparam0, 9, NULL);
-	xTaskCreate(&server_handle_task, "server_handle_task", 1024*3, NULL, 6, NULL);
 
-	// Create Timer (Trigger a measurement every second)
-	ESP_LOGI(TAG, "CONFIG_ADC_CYCLE=%d", CONFIG_ADC_CYCLE);
-	TimerHandle_t timerHandle = xTimerCreate("MY Trigger", CONFIG_ADC_CYCLE, pdTRUE, NULL, timer_cb);
-	if (timerHandle != NULL) {
-		if (xTimerStart(timerHandle, 0) != pdPASS) {
-			ESP_LOGE(TAG, "Unable to start Timer");
-			while(1) { vTaskDelay(1); }
-		} else {
-			ESP_LOGI(TAG, "Success to start Timer");
-		}
-	} else {
-		ESP_LOGE(TAG, "Unable to create Timer");
-		while(1) { vTaskDelay(1); }
-	}
+	// Start web server
+	xTaskCreate(&server_task, "server_task", 1024*4, (void *)cparam0, 5, NULL);
 
-	// ADC1 Calibration Init
-	adc_cali_handle_t adc1_cali_handle = NULL;
-	bool do_calibration = adc_calibration_init(ADC_UNIT_1, ADC_ATTEN_DB_11, &adc1_cali_handle);
-	if (do_calibration == false) {
-		ESP_LOGE(TAG, "calibration fail");
-		while(1) { vTaskDelay(1); }
-	}
+	// Start web client
+	xTaskCreate(&client_task, "client_task", 1024*4, NULL, 5, NULL);
 
-	// ADC1 Init
-	adc_oneshot_unit_handle_t adc1_handle;
-	adc_oneshot_unit_init_cfg_t init_config1 = {
-		.unit_id = ADC_UNIT_1,
-	};
-	ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
-
-	// ADC1 config
-	adc_oneshot_chan_cfg_t config = {
-		.bitwidth = ADC_BITWIDTH_DEFAULT,
-		.atten = ADC_ATTEN_DB_11,
-	};
-	adc_channel_t adc1_channel1 = gpio2adc(CONFIG_METER1_GPIO);
-	ESP_LOGI(TAG, "CONFIG_METER1_GPIO=%d adc1_channel1=%d", CONFIG_METER1_GPIO, adc1_channel1);
-	ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, adc1_channel1, &config));
-
-	char meter1[8];
-	sprintf(meter1, "GPIO%02d", CONFIG_METER1_GPIO);
-	char meter2[8];
-	memset(meter2, 0, sizeof(meter2));
-	char meter3[8];
-	memset(meter3, 0, sizeof(meter3));
-
-#if CONFIG_ENABLE_METER2
-	adc_channel_t adc1_channel2 = gpio2adc(CONFIG_METER2_GPIO);
-	ESP_LOGI(TAG, "CONFIG_METER2_GPIO=%d adc1_channel2=%d", CONFIG_METER2_GPIO, adc1_channel2);
-	ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, adc1_channel2, &config));
-	sprintf(meter2, "GPIO%02d", CONFIG_METER2_GPIO);
-#endif
-
-#if CONFIG_ENABLE_METER3
-	adc_channel_t adc1_channel3 = gpio2adc(CONFIG_METER3_GPIO);
-	ESP_LOGI(TAG, "CONFIG_METER3_GPIO=%d adc1_channel3=%d", CONFIG_METER3_GPIO, adc1_channel3);
-	ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, adc1_channel3, &config));
-	sprintf(meter3, "GPIO%02d", CONFIG_METER3_GPIO);
-#endif
-
-	char cRxBuffer[512];
-	char DEL = 0x04;
-	char outBuffer[64];
-
-	while (1) {
-		size_t readBytes = xMessageBufferReceive(xMessageBufferMain, cRxBuffer, sizeof(cRxBuffer), portMAX_DELAY );
-		ESP_LOGD(TAG, "readBytes=%d", readBytes);
-		cJSON *root = cJSON_Parse(cRxBuffer);
-		if (cJSON_GetObjectItem(root, "id")) {
-			char *id = cJSON_GetObjectItem(root,"id")->valuestring;
-			ESP_LOGD(TAG, "id=[%s]",id);
-
-			if ( strcmp (id, "init") == 0) {
-				//sprintf(outBuffer,"HEAD%cADC Channel is %d", DEL, adc1_channel1);
-				sprintf(outBuffer,"HEAD%cAnalog Value Display using ESP32", DEL);
-				ESP_LOGD(TAG, "outBuffer=[%s]", outBuffer);
-				ws_server_send_text_all(outBuffer,strlen(outBuffer));
-
-				sprintf(outBuffer,"METER%c%s%c%s%c%s", DEL,meter1,DEL,meter2,DEL,meter3);
-				ESP_LOGD(TAG, "outBuffer=[%s]", outBuffer);
-				ws_server_send_text_all(outBuffer,strlen(outBuffer));
-			} // end if
-
-			if ( strcmp (id, "timer-request") == 0) {
-				int voltage1 = 0;
-				int32_t adc_reading1 = 0;
-				int adc_raw;
-				for (int i = 0; i < NO_OF_SAMPLES; i++) {
-					ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, adc1_channel1, &adc_raw));
-					adc_reading1 += adc_raw;
-				}
-				adc_reading1 /= NO_OF_SAMPLES;
-				ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_reading1, &voltage1));
-#if CONFIG_ENABLE_STDOUT
-				ESP_LOGI(TAG, "GPIO%02d adc1_channel1: %d Raw: %"PRIi32" Voltage: %dmV", CONFIG_METER1_GPIO, adc1_channel1, adc_reading1, voltage1);
-#endif
-
-				int voltage2 = 0;
-#if CONFIG_ENABLE_METER2
-				int32_t adc_reading2 = 0;
-				for (int i = 0; i < NO_OF_SAMPLES; i++) {
-					ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, adc1_channel2, &adc_raw));
-					adc_reading2 += adc_raw;
-				}
-				adc_reading2 /= NO_OF_SAMPLES;
-				ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_reading2, &voltage2));
-#if CONFIG_ENABLE_STDOUT
-				ESP_LOGI(TAG, "GPIO%02d adc1_channel2: %d Raw: %"PRIi32" Voltage: %dmV", CONFIG_METER2_GPIO, adc1_channel2, adc_reading2, voltage2);
-#endif
-#endif // CONFIG_ENABLE_METER2
-
-				int voltage3 = 0;
-#if CONFIG_ENABLE_METER3
-				int32_t adc_reading3 = 0;
-				for (int i = 0; i < NO_OF_SAMPLES; i++) {
-					ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, adc1_channel3, &adc_raw));
-					adc_reading3 += adc_raw;
-				}
-				adc_reading3 /= NO_OF_SAMPLES;
-				ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_reading3, &voltage3));
-#if CONFIG_ENABLE_STDOUT
-				ESP_LOGI(TAG, "GPIO%02d adc1_channel3: %d Raw: %"PRIi32" Voltage: %dmV", CONFIG_METER3_GPIO, adc1_channel3, adc_reading3, voltage3);
-#endif
-#endif // CONFIG_ENABLE_METER3
-
-				sprintf(outBuffer,"DATA%c%d%c%d%c%d", DEL, voltage1, DEL, voltage2, DEL, voltage3);
-				ESP_LOGD(TAG, "outBuffer=[%s]", outBuffer);
-				ws_server_send_text_all(outBuffer,strlen(outBuffer));
-
-				ESP_LOGD(TAG,"free_size:%d %d", heap_caps_get_free_size(MALLOC_CAP_8BIT), heap_caps_get_free_size(MALLOC_CAP_32BIT));
-
-			} // end if
-
-		} // end if
-
-		// Delete a cJSON structure
-		cJSON_Delete(root);
-
-	} // end while
+	vTaskDelay(100);
 }
